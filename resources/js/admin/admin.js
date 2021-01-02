106f442e31a7359f35e8dfdc33c726c0ba4952e3
Dendi Handian <dendihandian@gmail.com>
2021-01-02 17:28:36 +0700

    adding sbadmin

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -1,1 +1,10 @@
 require('./bootstrap');
+
+// SBAdmin Scripts
+require('startbootstrap-sb-admin-2/vendor/jquery/jquery.slim'); // minimal jquery
+// require('startbootstrap-sb-admin-2/vendor/jquery/jquery'); // or use this instead if you want full jquery
+require('startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing');
+require('startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle');
+require('startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4');
+// require('startbootstrap-sb-admin-2/vendor/datatables');
+require('startbootstrap-sb-admin-2/js/sb-admin-2');
