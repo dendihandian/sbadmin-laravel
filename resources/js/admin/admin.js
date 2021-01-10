383d09ddb2d7297b74aaea53bb90b2ccb795c57c
Dendi Handian <dendihandian@gmail.com>
2021-01-10 19:51:41 +0700

    all sbadmin dependencies

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -1,4 +1,1 @@
 require('./bootstrap');
-
-require('startbootstrap-sb-admin-2/vendor/datatables/jquery.dataTables');
-require('startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4');
--- resources/js/admin/bootstrap.js
+++ resources/js/admin/bootstrap.js
@@ -5,7 +5,10 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
+    require('jquery.easing');
     require('datatables.net-bs4')( window, $ );
+    // require('chart.js/dist/Chart.bundle');
+    require('startbootstrap-sb-admin-2/js/sb-admin-2');
 
 } catch (error) {
     console.log(error);
