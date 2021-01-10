fb31b2bb1b7ad4c59713c09efa645d9b35769b2d
Dendi Handian <dendihandian@gmail.com>
2021-01-10 18:17:52 +0700

    ignore build resources

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -1,1 +1,4 @@
 require('./bootstrap');
+
+require('startbootstrap-sb-admin-2/vendor/datatables/jquery.dataTables');
+require('startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4');
--- resources/js/admin/bootstrap.js
+++ resources/js/admin/bootstrap.js
@@ -1,15 +1,11 @@
-// window._ = require('lodash');
+window._ = require('lodash');
 
 try {
-    window.$ = window.jQuery = require('startbootstrap-sb-admin-2/vendor/jquery/jquery.slim'); // minimal jquery
-    // window.$ = window.jQuery = require('startbootstrap-sb-admin-2/vendor/jquery/jquery'); // or use this instead if you want full jquery
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
 
-    // sbadmin script
-    require('startbootstrap-sb-admin-2/vendor/jquery-easing/jquery.easing');
-    require('startbootstrap-sb-admin-2/vendor/bootstrap/js/bootstrap.bundle');
-    require('startbootstrap-sb-admin-2/vendor/datatables/dataTables.bootstrap4');
-    // require('startbootstrap-sb-admin-2/vendor/datatables');
-    require('startbootstrap-sb-admin-2/js/sb-admin-2');
+    require('bootstrap');
+    require('datatables.net-bs4')( window, $ );
 
 } catch (error) {
     console.log(error);
