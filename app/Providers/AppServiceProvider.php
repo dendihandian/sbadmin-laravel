056d4b277a9310ee81bac58622cb3529e9e3fa25
Dendi Handian <handian.dendi@gmail.com>
2021-06-25 00:27:39 +0700

    fix app provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $urlGenerator)
+    public function boot()
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
