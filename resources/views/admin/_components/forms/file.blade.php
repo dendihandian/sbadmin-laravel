17f3c195d19919d02fdb9362db46dde52c6f59a1
Dendi Handian <handian.dendi@gmail.com>
2021-06-26 17:29:27 +0700

    filepond load seems working

--- resources/views/admin/_components/forms/file.blade.php
+++ resources/views/admin/_components/forms/file.blade.php
@@ -1,5 +1,6 @@
 <div class="form-group col-lg-{{ $_col ?? 6 }}">
     <label for="{{ $_name }}">{{ $_title ?? '' }}</label>
+    <input type="hidden" name="{{ $_name }}-value" value="{{ $_value ?? null }}">
     <input class="form-control-file @error($_name) is-invalid @enderror" type="file" name="{{ $_name }}" id="{{ $_name }}" value="{{ $_value ?? null }}">
     @error($_name)
         <span class="invalid-feedback" user="alert"><strong>{{ $message }}</strong></span>
